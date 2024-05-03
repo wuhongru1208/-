22c1b36d6d630b57ff62a80711925d41e212e90f
wuhongru1208 <161403493+wuhongru1208@users.noreply.github.com>
2024-05-03 16:00:51 +0800

    Add files via upload

--- 123.py
+++ 123.py
@@ -1,17 +1,34 @@
 import pygame
+import tkinter as tk
+window = tk.Tk()
+pygame.init()
 
-pygame.mixer.init()
+pygame.mixer.music.load("music.mp3")
 
-clock = pygame.time.Clock()
+started = False
+playing = False
 
-crash = pygame.mixer.Sound('12274.wav')
-hit = pygame.mixer.Sound('hit.wav')
-
-count = 0
-while count < 200:
-    if count % 4 == 0:
-        crash.play()
+def buttonClick():
+    global playing,started
+    if not playing:
+        if not started:
+            pygame.mixer.music.play(-1)
+            started = True
+        else:
+            pygame.mixer.music.unpause()
+        button.config(text="Pause")
     else:
-        hit.play()
-    count += 1
-    clock.tick(2)
+        pygame.mixer.music.pause()
+        button.config(text="Play")
+    playing = not playing
+
+def setVolume(val):
+    volume = float(slider.get())
+    pygame.mixer.music.set_volume(volume/100)
+
+slider = tk.Scale(window,from_= 100 , to = 0,command = setVolume)
+button = tk.Button(window,text = "Play",command=buttonClick)
+slider.pack()
+slider.set(100)
+button.pack()
+window.mainloop()
